622f8a14aa5ff2ceaf43534aa634aa1eb868cb04
Ajith M <98872197+ajithm7@users.noreply.github.com>
2022-04-08 17:45:00 +0530

    Update Area.java

--- Area.java
+++ Area.java
@@ -8,30 +8,55 @@ package com.program75;
 import java.util.Scanner;
 
 public class Area {
+	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		// square
+		Area area = new Area();
+		area.areOption();
+
+	}
+
+	public void areOption() {
+		System.out.println("Select any one below :\n1->Square\n2->Rectangle\n3->Cylinder\n4->Two concentric circle");
+		char ch = sc.next().charAt(0);
+		switch (ch) {
+		case '1' -> areaOfSquare();
+		case '2' -> areaOfRectangle();
+		case '3' -> areaOfCylinder();
+		case '4' -> areaOfTwoConcentricCircle();
+		default -> System.err.println("Enter valid selection");
+		}
+	}
+
+	// square
+	public void areaOfSquare() {
 		System.out.println("Enter side value of square:");
 		float sqArea, sqSide = sc.nextFloat();
 		sqArea = sqSide * sqSide;
 		System.out.println("Area of the square=" + sqArea);
+	}
 
-		// rectangular
+	// rectangular
+	public void areaOfRectangle() {
 		System.out.println("Enter Width and Lengtgh value of rectangle:");
 		int w = sc.nextInt();
 		int l = sc.nextInt();
 		float recArea = w * l;
 		System.out.println("Area of rectangle=" + recArea);
+	}
 
-		// cylinder
+	// cylinder
+	public void areaOfCylinder() {
+
 		System.out.println("Enter the radius ans height of the cylinder");
 		double r = sc.nextDouble();
 		double h = sc.nextDouble();
 		double cylinderArea = (2 * Math.PI) * (r * r) * (r + h);
 		System.out.println("Area of the cylinder is :" + cylinderArea);
+	}
 
-		// two concentric circle
+	// two concentric circle
+	public void areaOfTwoConcentricCircle() {
 		System.out.println("Enter the langer circle radius:");
 		double r1 = sc.nextDouble();
 		System.out.println("Enter the smaller circle radius:");
